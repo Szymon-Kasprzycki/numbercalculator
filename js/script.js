4ec3a0505b6b218512b34069727ec6e8a10041ba
Szymon Kasprzycki <66119938+Szymon-Kasprzycki@users.noreply.github.com>
2020-10-11 23:19:53 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 let wynik;
-let x;
 
 function CheckRadio() {
     var RadioButtons = document.querySelectorAll(".numberfrom");
     for (let i = 0; i < RadioButtons.length; i++) {
         if (RadioButtons[i].checked) {
-            const myNumber = document.querySelector('#number-from').value;
+            let myNumber = document.querySelector('#number-from').value;
             let power = 0;
             wynik = 0;
             if (RadioButtons[i].value == 2) {
                 for (let i = (myNumber.length - 1); i >= 0; i--) {
-                    // while (x < myNumber.length) {
                     const binary = [0, 1]
                     if (binary.indexOf(parseInt(myNumber[i], 10)) == -1) {
                         alert("Liczba " + myNumber + " nie jest liczbą binarną!");
-                        x = myNumber.length;
                         wynik = "";
                         return;
                     }
@@ -27,11 +24,9 @@ function CheckRadio() {
 
             if (RadioButtons[i].value == 4) {
                 for (let i = (myNumber.length - 1); i >= 0; i--) {
-                    // while (x < myNumber.length) {
                     const quad = [0, 1, 2, 3]
                     if (quad.indexOf(parseInt(myNumber[i], 10)) == -1) {
                         alert("Liczba " + myNumber + " nie jest liczbą w systemie czwórkowym!");
-                        x = myNumber.length;
                         wynik = "";
                         return;
                     }
@@ -43,11 +38,9 @@ function CheckRadio() {
             }
             if (RadioButtons[i].value == 8) {
                 for (let i = (myNumber.length - 1); i >= 0; i--) {
-                    // while (x < myNumber.length) {
                     const octal = [0, 1, 2, 3, 4, 5, 6, 7]
                     if (octal.indexOf(parseInt(myNumber[i], 10)) == -1) {
                         alert("Liczba " + myNumber + " nie jest liczbą w systemie ósemkowym!");
-                        x = myNumber.length;
                         wynik = "";
                         return;
                     }
@@ -57,14 +50,12 @@ function CheckRadio() {
                     }
                 }
             }
-            // DO DOPRACOWANIA
             if (RadioButtons[i].value == 16) {
+                myNumber = myNumber.toUpperCase();
                 for (let i = (myNumber.length - 1); i >= 0; i--) {
-                    // while (x < myNumber.length) {
-                    const hexaDecimal = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-                    if (hexaDecimal.indexOf(parseInt(myNumber[i], 10)) == -1) {
+                    const hexaDecimal = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+                    if (hexaDecimal.indexOf(myNumber[i]) == -1) {
                         alert("Liczba " + myNumber + " nie jest liczbą w systemie szesnastkowym! (litery w liczbie powinny być zapisane jako wielkie!!!)");
-                        x = myNumber.length;
                         wynik = "";
                         return;
                     }
@@ -82,7 +73,7 @@ function CheckRadio() {
                         return;
                     }
                     else {
-                        wynik = parseInt(myNumber);
+                        wynik = parseInt(myNumber, 10);
                     }
                 }
             }
